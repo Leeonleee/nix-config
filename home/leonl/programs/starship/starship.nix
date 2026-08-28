f6039450299d709c041123a53bd8218aa9e26a38
Leon Lee <leonlee20031219@gmail.com>
2026-08-28 13:53:21 +1000

    starship config stuff

--- home/leonl/programs/starship/starship.nix
+++ home/leonl/programs/starship/starship.nix
@@ -3,5 +3,10 @@
 {
   programs.starship = {
     enable = true;
+
+    enableZshIntegration = true;
+    enableBashIntegration = true;
   };
+
+  xdg.configFile."starship.toml".source = ./starship.toml;
 }
