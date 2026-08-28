8023fa6c4e10f69ded077d11a273ed1a8f373128
Leon Lee <leonlee20031219@gmail.com>
2026-08-28 10:59:59 +1000

    changed flake file directory to new path

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          home-manager.users.leonl = import ./home/leon.nix;
+          home-manager.users.leonl = import ./home/leonl/home.nix;
         }
       ];
     };
