77d7c21e0fc3cbec47203f6badc5b30371bd7db0
Leon Lee <leonlee20031219@gmail.com>
2026-08-28 10:27:04 +1000

    Initial NixOS configuration

---
+++ home/leon.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "leonl";
+  home.homeDirectory = "/home/leonl";
+
+  home.stateVersion = "26.05";
+  
+  home.packages = with pkgs; [
+  ];
+
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user = {
+        name = "Leon Lee";
+	email = "leonlee20031219@gmail.com";
+      };
+
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake ~/nix-config#framework";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.home-manager.enable = true;
+}
