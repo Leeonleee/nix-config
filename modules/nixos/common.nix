0d06c119b35a4f1a8be445f63375ad782a98cec8
Leon Lee <leonlee20031219@gmail.com>
2026-08-29 16:08:32 +1000

    phase 1 of refactoring repo

---
+++ modules/nixos/common.nix
@@ -0,0 +1,74 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ../theme.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.networkmanager.enable = true;
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  time.timeZone = "Australia/Sydney";
+
+  i18n.defaultLocale = "en_AU.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services.tailscale.enable = true;
+
+  users.users.leonl = {
+    isNormalUser = true;
+    description = "Leon Lee";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+    shell = pkgs.zsh;
+  };
+
+  programs.firefox.enable = true;
+  programs.zsh.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "26.05";
+}
