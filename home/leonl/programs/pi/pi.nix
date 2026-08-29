c4734246ea85c7270feabbc4c3935a8708024d7c
Leon Lee <leonlee20031219@gmail.com>
2026-08-29 15:58:17 +1000

    added support for pi packages

--- home/leonl/programs/pi/pi.nix
+++ home/leonl/programs/pi/pi.nix
@@ -1,12 +1,41 @@
-{ inputs, pkgsUnstable, ... }:
+{ pkgs, pkgsUnstable, config, ... }:
+
+let
+  wrappedPi = pkgs.symlinkJoin {
+    name = "pi-coding-agent";
+
+    paths = [
+      pkgsUnstable.pi-coding-agent
+    ];
+
+    nativeBuildInputs = [
+      pkgs.makeWrapper
+    ];
+
+    postBuild = ''
+      wrapProgram $out/bin/pi \
+        --set NPM_CONFIG_PREFIX ${config.home.homeDirectory}/.pi/npm
+    '';
+  };
+in
 {
   programs.pi-coding-agent = {
     enable = true;
-    package = pkgsUnstable.pi-coding-agent;
+
+    package = wrappedPi;
+
+    extraPackages = [
+      pkgs.nodejs_latest
+    ];
 
     settings = {
-      defaultProvider = "openai";
+      defaultProvider = "openai-codex";
       defaultThinkingLevel = "high";
+      defaultModel = "gpt-5.6-sol";
+
+      packages = [
+        "npm:pi-init"
+      ];
     };
   };
 }
