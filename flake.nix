bb286f27680ac7070ec2ef45005cb33f490ca21c
Leon Lee <leonlee20031219@gmail.com>
2026-09-26 00:15:06 +1000

    added pc to dev-nix ssh authorised keys

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.numtide.com"
+      "https://cache.numtide.com?priority=50"
     ];
     extra-trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
